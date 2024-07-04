24a984406698e3c0f1d797219d149580a55db768
DoctorHCode333 <harshvardhanbawake@gmail.com>
2024-07-04 15:09:13 +0530

    init

---
+++ src/clientConfig.jsx
@@ -0,0 +1,5 @@
+export const clientConfig = {
+    clientId: 'e0b35ccc-9d53-4f49-9850-902b2a2c4213',
+    redirectUri: 'http://localhost:3000',
+    offlinePresenceId: 'ccf3c10a-aa2c-4845-8e8d-f59fa48c58e5',
+}
