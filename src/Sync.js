67580ea7c76c3515fca8a5d4665c21859865cdfa
HARSHVARDHAN BAWAKE <94169362+DoctorHCode333@users.noreply.github.com>
2024-10-10 15:44:34 +0530

    Update Sync.js

--- src/Sync.js
+++ src/Sync.js
@@ -226,3 +226,153 @@ const HeaderFilterChips = ({
 
 export default HeaderFilterChips;
 
+///////////////////////////////////////////////////////////////////
+
+
+terraform {
+  required_providers {
+    genesyscloud = {
+      source = "mypurecloud/genesyscloud"
+
+    }
+  }
+}
+
+###
+#
+# Description:  
+#
+# Creates a new web-based data integration and then adds a data action that calls out to the AWS API Gateway endpoint and 
+# Lambda that is performing our email-classification. 
+#
+### 
+
+resource "genesyscloud_integration" "ComprehendDataAction" {
+  intended_state   = "ENABLED"
+  integration_type = "custom-rest-actions"
+  config {
+    name       = "ComprehendDataAction"
+    properties = jsonencode({})
+    advanced   = jsonencode({})
+    notes      = "Used to invoke an AWS Comprehend job"
+  }
+}
+
+resource "genesyscloud_integration_action" "LookupQueueName" {
+  name           = "LookupQueueName"
+  category       = "ComprehendDataAction"
+  integration_id = genesyscloud_integration.ComprehendDataAction.id
+  secure         = false
+  contract_input = jsonencode({
+    "type"     = "object",
+    "required" = ["EmailSubject", "EmailBody"],
+    "properties" = {
+      "EmailSubject" = {
+        "type" = "string"
+      },
+      "EmailBody" = {
+        "type" = "string"
+      }
+    }
+  })
+  
+  contract_output = jsonencode({
+    "type" = "object",
+    "required" = [
+      "QueueName"
+    ],
+    "properties" = {
+      "QueueName" = {
+        "type" = "string"
+      }
+    }
+  })
+  config_request {
+    request_url_template = var.classifier_url
+    request_type         = "POST"
+    request_template     = "$${input.rawRequest}"
+    headers = {
+      x-amazon-apigateway-api-key-source = "HEADER"
+      X-API-Key                          = var.classifier_api_key
+    }
+  }
+  config_response {
+    translation_map          = {}
+    translation_map_defaults = {}
+    success_template         = "$${rawResult}"
+  }
+}
+
+
+{
+  "name": "Get Estimated Wait Time - Exported 2024-10-10 @ 15:33",
+  "integrationType": "purecloud-data-actions",
+  "actionType": "static",
+  "config": {
+    "request": {
+      "requestUrlTemplate": "/api/v2/routing/queues/${input.QUEUE_ID}/mediatypes/${input.MEDIA_TYPE}/estimatedwaittime",
+      "requestType": "GET",
+      "headers": {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "UserAgent": "PureCloudIntegrations/1.0"
+      },
+      "requestTemplate": "${input.rawRequest}"
+    },
+    "response": {
+      "translationMap": {
+        "estimated_wait_time": "$.results[0].estimatedWaitTimeSeconds"
+      },
+      "translationMapDefaults": {},
+      "successTemplate": "{\n   \"estimated_wait_time\": ${estimated_wait_time}\n}"
+    }
+  },
+  "contract": {
+    "input": {
+      "inputSchema": {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "title": "Estimated Wait Time Request",
+        "description": "The estimated wait time for a specific media type and queue.",
+        "type": "object",
+        "required": [
+          "QUEUE_ID",
+          "MEDIA_TYPE"
+        ],
+        "properties": {
+          "QUEUE_ID": {
+            "type": "string",
+            "description": "The queue ID."
+          },
+          "MEDIA_TYPE": {
+            "type": "string",
+            "description": "The media type of the interaction: call, chat, callback, email, social media, video communication, or message.",
+            "enum": [
+              "call",
+              "chat",
+              "callback",
+              "email",
+              "socialExpression",
+              "videoComm",
+              "message"
+            ]
+          }
+        }
+      }
+    },
+    "output": {
+      "successSchema": {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "title": "Get Estimated Wait Time Response",
+        "description": "Returns the estimated wait time.",
+        "type": "object",
+        "properties": {
+          "estimated_wait_time": {
+            "type": "integer",
+            "title": "Estimated Wait Time in Seconds",
+            "description": "The estimated wait time (in seconds) for the specified media type and queue. "
+          }
+        }
+      }
+    }
+  },
+  "secure": false
+}
